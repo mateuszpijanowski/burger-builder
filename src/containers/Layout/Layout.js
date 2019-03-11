bd4a0fbaa753247e9f48c3e6d047828189e1820f
mateuszpijanowski <mateuszpijanowski0@gmail.com>
2019-03-11 18:09:29 +0100

    Finish adding About page

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux/aux';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import Modal from '../../components/UI/Modal/Modal';
 import classes from './Layout.css';
+import About from '../../components/UI/Modal/About/About';
 
 import { connect } from 'react-redux';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: false
-    };
-
-    sideDrawerClosedHandler = () => {
-        this.setState({ showSideDrawer: false })
+        showSideDrawer: false,
+        showAbout: false
     };
 
     sideDrawerToggleHandler = () => {
@@ -21,15 +20,36 @@ class Layout extends Component {
         });
     };
 
+    aboutToggleHandler = () => {
+        if (this.state.showSideDrawer) {
+            this.setState({ showSideDrawer: false });
+        }
+
+        this.setState((prevState) => {
+            return { showAbout: !prevState.showAbout };
+        });
+    };
+
     render() {
+        const aboutComponent = (
+            <Modal
+                show={this.state.showAbout}
+                modalClosed={this.aboutToggleHandler}>
+                <About />
+            </Modal>
+        );
+
         return (
             <Aux>
+                { this.state.showAbout ? aboutComponent : null }
                 <Toolbar
                     isAuth={this.props.isAuth}
+                    aboutToggle={this.aboutToggleHandler}
                     drawerToggleClicked={this.sideDrawerToggleHandler} />
                 <SideDrawer
                     isAuth={this.props.isAuth}
-                    closed={this.sideDrawerClosedHandler}
+                    aboutToggle={this.aboutToggleHandler}
+                    closed={this.sideDrawerToggleHandler}
                     open={this.state.showSideDrawer} />
                 <main className={classes.Content}>
                     {this.props.children}
